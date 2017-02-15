848fa2a6791d01b899898473de0fd3f1735ba5c7
Chingis Sandanov <chingis@wodby.com>
2017-02-15 10:32:43 +0700

    Replace sed with tail

--- 7.0/test.sh
+++ 7.0/test.sh
@@ -5,14 +5,12 @@ set -ex
 function checkPhpModules {
     # Export PHP modules.
     make run -e CMD="php -m" ENV="-e PHP_XDEBUG=1" > ./test/php_modules.tmp
-    sed --v
-    cat test/php_modules.tmp
-    sed '1d; $d' test/php_modules.tmp > test/tmp
-    cat test/tmp
-    mv test/tmp test/php_modules.tmp
+    tail -n +2 ./test/php_modules.tmp > ./test/tmp
+    cat ./test/tmp
+    mv ./test/tmp ./test/php_modules.tmp
     # Compare PHP modules.
     if ! cmp ./test/php_modules.tmp ./test/php_modules; then
-        echo 'Error. PHP modules is not identical.'
+        echo 'Error. PHP modules are not identical.'
         diff ./test/php_modules.tmp ./test/php_modules
         exit 1
     fi
