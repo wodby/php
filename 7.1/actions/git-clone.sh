2801e02b52618a7ce886707e0b09be25abc77a84
Chingis Sandanov <chingis@wodby.com>
2017-04-19 12:40:10 +0700

    Set global git user email and name

--- 7.1/actions/git-clone.sh
+++ 7.1/actions/git-clone.sh
@@ -8,5 +8,10 @@ fi
 
 URL=$1
 BRANCH=$2
+USER_EMAIL=$3
+USER_NAME=$4
+
+git config --global user.email "${USER_EMAIL}"
+git config --global user.name "${USER_NAME}"
 
 git clone -b "${BRANCH}" "${URL}" "${APP_ROOT}"
