fe9e7aab598ab5b9b2dda83195bef549f04ae539
Chingis Sandanov <chingis@wodby.com>
2017-09-28 20:04:55 +0700

    Debug travis tests

--- 7.1/test.sh
+++ 7.1/test.sh
@@ -37,6 +37,8 @@ run_action() {
 }
 
 docker-compose -f test/docker-compose.yml up -d
+docker-compose -f test/docker-compose.yml logs
+
 run_action nginx check-ready max_try=10
 run_action php check-ready max_try=10
 
