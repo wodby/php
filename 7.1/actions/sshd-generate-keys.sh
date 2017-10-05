cd6242b2c6d3b7fd300f77233e02a357b3ced8e6
Chingis Sandanov <chingis@wodby.com>
2017-10-05 19:23:55 +0700

    Generate only rsa key for sshd

--- 7.1/actions/sshd-generate-keys.sh
+++ 7.1/actions/sshd-generate-keys.sh
@@ -9,9 +9,6 @@ fi
 dir="${1:-/etc/ssh}"
 
 mkdir -p "${dir}"
-yes "y" | ssh-keygen -qb 2048 -t rsa -N "" -f "${dir}/ssh_host_rsa_key"
-yes "y" | ssh-keygen -qt dsa -N '' -f "${dir}/ssh_host_dsa_key"
-yes "y" | ssh-keygen -qt ecdsa -N '' -f "${dir}/ssh_host_ecdsa_key"
-yes "y" | ssh-keygen -qt ed25519 -N '' -f "${dir}/ssh_host_ed25519_key"
-chmod 0600 "${dir}"/*_key
-chmod 0644 "${dir}"/*.pub
+ssh-keygen -qb 2048 -t rsa -N "" -f "${dir}/ssh_host_rsa_key"
+chmod 0600 "${dir}/ssh_host_rsa_key"
+chmod 0644 "${dir}/ssh_host_rsa_key.pub"
