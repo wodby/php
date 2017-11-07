11657d5685ba76270fd5fad37129d11941c4813e
Michael Crump <mcrump001@gmail.com>
2017-11-07 17:34:57 -0500

    Check for regular file instead of existence

--- 7.1/actions/sshd-generate-keys.sh
+++ 7.1/actions/sshd-generate-keys.sh
@@ -9,7 +9,7 @@ fi
 dir="${1:-/etc/ssh}"
 
 mkdir -p "${dir}"
-if [[ ! -e "${dir}/ssh_host_rsa_key" ]]; then
+if [[ ! -f "${dir}/ssh_host_rsa_key" ]]; then
     ssh-keygen -qb 2048 -t rsa -N "" -f "${dir}/ssh_host_rsa_key"
 fi
 chmod 0600 "${dir}/ssh_host_rsa_key"
