051ccfffe7d77e654a861aa8d13b3279f172c3f9
Chingis Sandanov <chingis@wodby.com>
2018-02-16 17:06:19 +0700

    Permissions fixes

--- 5.3/actions/files-import.sh
+++ 5.3/actions/files-import.sh
@@ -15,6 +15,8 @@ get-archive.sh "${source}" "${tmp_dir}" "zip tgz tar.gz tar"
 if [[ -f "${tmp_dir}/.wodby" || (-d "${tmp_dir}/private" && -d "${tmp_dir}/public") ]]; then
     echo "Wodby backup archive detected. Importing to top directory"
     rsync -rlt --chown=www-data:www-data "${tmp_dir}/" "${FILES_DIR}"
+    # Ensure files volume permissions is still correct.
+    init-volumes.sh
 else
     echo "Importing files to public directory"
     rsync -rlt --chown=www-data:www-data "${tmp_dir}/" "${FILES_DIR}/public/"
