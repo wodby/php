3a29e005fcd7341f613049607d7d12250f72ef5c
Chingis Sandanov <chingis@wodby.com>
2018-08-13 18:40:28 +0700

    Minor fix

--- 5.6/docker-entrypoint.sh
+++ 5.6/docker-entrypoint.sh
@@ -97,7 +97,7 @@ fi
 
 exec_init_scripts
 
-if [[ $1 == "make" ]]; then
+if [[ "${1}" == "make" ]]; then
     exec "${@}" -f /usr/local/bin/actions.mk
 else
     exec /usr/local/bin/docker-php-entrypoint "${@}"
