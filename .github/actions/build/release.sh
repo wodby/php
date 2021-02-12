37564147ac08651f4d2f21743b563e0a2cc91f35
Chingis Sandanov <chingis@wodby.com>
2021-02-12 09:39:40 +0800

    Use buildx for -dev-macos

---
+++ .github/actions/build/release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+if [[ "${GITHUB_REF}" == refs/heads/master || "${GITHUB_REF}" == refs/tags/* ]]; then
+    docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+
+    if [[ "${GITHUB_REF}" == refs/tags/* ]]; then
+      export STABILITY_TAG="${GITHUB_REF##*/}"
+    fi
+
+    IFS=',' read -ra tags <<< "${TAGS}"
+
+    for tag in "${tags[@]}"; do
+        make release TAG="${tag}";
+    done
+fi
