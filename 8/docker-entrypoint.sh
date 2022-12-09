62681d305153e779c59069a44f4142585d88ab58
Chingis Sandanov <chingis@wodby.com>
2022-12-09 18:32:18 +0300

    Add PHP 8.2, drop PHP 7, update pecl extensions

--- 8/docker-entrypoint.sh
+++ 8/docker-entrypoint.sh
@@ -61,10 +61,12 @@ process_templates() {
     fi
 
     # Extensions that don't work with --enabled-debug
-    _gotpl "docker-php-ext-newrelic.ini.tmpl" "${PHP_INI_DIR}/conf.d/docker-php-ext-newrelic.ini"
+    if [[ "${php_ver_minor}" == "8.1" ]]; then
+      _gotpl "docker-php-ext-newrelic.ini.tmpl" "${PHP_INI_DIR}/conf.d/docker-php-ext-newrelic.ini"
+    fi
 
+    _gotpl "docker-php.ini.tmpl" "${PHP_INI_DIR}/conf.d/docker-php.ini"
     _gotpl "docker-php-ext-apcu.ini.tmpl" "${PHP_INI_DIR}/conf.d/docker-php-ext-apcu.ini"
-    _gotpl "docker-php-${php_ver_minor}.ini.tmpl" "${PHP_INI_DIR}/conf.d/docker-php.ini"
     _gotpl "docker-php-ext-brotli.ini.tmpl" "${PHP_INI_DIR}/conf.d/docker-php-ext-brotli.ini"
     _gotpl "docker-php-ext-igbinary.ini.tmpl" "${PHP_INI_DIR}/conf.d/docker-php-ext-igbinary.ini"
     _gotpl "docker-php-ext-xhprof.ini.tmpl" "${PHP_INI_DIR}/conf.d/docker-php-ext-xhprof.ini"
