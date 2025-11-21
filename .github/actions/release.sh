0dfe8f5b005b1be5ef8403e0be5e4c9e3f8a60fa
Chingis Sandanov <chingis@chingis.io>
2025-11-21 22:48:37 +0800

    Workflow simplifications

--- .github/actions/release.sh
+++ .github/actions/release.sh
@@ -10,30 +10,30 @@ if [[ "${GITHUB_REF}" == refs/heads/master || "${GITHUB_REF}" == refs/tags/* ]];
   if [[ -n "${PHP_DEV}" ]]; then            
     if [[ "${WODBY_USER_ID}" == "501" ]]; then
       minor_tag="${minor_tag}-dev-macos"
-      if [[ "${LATEST_MAJOR}" == "true" ]]; then    
+      if [[ -n "${LATEST_MAJOR}" ]]; then
         major_tag="${major_tag}-dev-macos"
       fi
     else 
       minor_tag="${minor_tag}-dev"
-      if [[ "${LATEST_MAJOR}" == "true" ]]; then
+      if [[ -n "${LATEST_MAJOR}" ]]; then
         major_tag="${major_tag}-dev"
       fi
     fi
   fi
 
   tags=("${minor_tag}")
-  if [[ "${LATEST_MAJOR}" == "true" ]]; then
+  if [[ -n "${LATEST_MAJOR}" ]]; then
      tags+=("${major_tag}")
   fi
 
   if [[ "${GITHUB_REF}" == refs/tags/* ]]; then
     stability_tag="${GITHUB_REF##*/}"
     tags=("${minor_tag}-${stability_tag}")
-    if [[ "${LATEST_MAJOR}" == "true" ]]; then
+    if [[ -n "${LATEST_MAJOR}" ]]; then
       tags+=("${major_tag}-${stability_tag}")
     fi
   else          
-    if [[ "${LATEST}" == "true" ]]; then
+    if [[ -n "${LATEST}" ]]; then
       if [[ -z "${PHP_DEV}" ]]; then
         tags+=("latest")
       else
